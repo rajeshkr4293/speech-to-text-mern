e0f309a7d509516b5f6be023849ea24e231ce532
Rajesh K R <krr04395@gmail.com>
2026-01-20 14:52:37 +0530

    Day 2: Express backend with multer file upload API

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const multer = require("multer");
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  }
+});
+
+const upload = multer({ storage });
+
+router.post("/upload", upload.single("audio"), (req, res) => {
+  res.json({
+    message: "Audio file uploaded successfully",
+    file: req.file
+  });
+});
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const uploadRoutes = require("./routes/uploadRoutes");
+
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running ");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.use("/api", uploadRoutes);
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
